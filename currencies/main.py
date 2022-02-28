456f6a29320528683952b7eea328b849bcd4ad99
Hasan <istamqulov@gmail.com>
2022-02-28 15:22:32 +0500

    Added homework for day 6

--- currencies/main.py
+++ currencies/main.py
@@ -1,35 +1,56 @@
 import requests
 import datetime
 
-URL = "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies.json"
-CURRENCY_URL = 'https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/%s/currencies/%s.json'
 
 
-def load_currencies():
-    resp = requests.get(URL)
+def get(url, ):
+    resp = requests.get(url)
     return resp.json()
 
 
-def get_currencies_for(currency_code, date: str = "latest"):
-    url = CURRENCY_URL % (date, currency_code)
+class Currencies(object):
+    CURRENCIES_URL = "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies.json"
+    CURRENCY_URL = 'https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/%s/currencies/%s.json'
 
-    data = requests.get(url).json()
-    return data[currency_code]
+    def load_currencies(self):
+        return get(self.CURRENCIES_URL)
 
+    def get_currencies_for(self, currency_code, date: str = "latest"):
+        url = self.CURRENCY_URL % (date, currency_code)
+        data = get(url)
+        return data[currency_code]
 
-def convert_currencies(base_currency, target_currency, amount):
-    rates = get_currencies_for(base_currency)
-    rate = rates[target_currency]
-    return rate * amount
+    def convert_currencies(self, base_currency, target_currency, amount):
+        rates = self.get_currencies_for(base_currency)
+        rate = rates[target_currency]
+        return rate * amount
+
+    def compare_currency_rate(self, base_currency, target_currency, start_date, end_date):
+        start_rate = self.get_currencies_for(base_currency, start_date)
+        end_rate = self.get_currencies_for(base_currency, end_date)
+        return end_rate[target_currency] - start_rate[target_currency]
+
+    def get_rates_history(self, base_currency, target_currency, days=10):
+        current_time = datetime.datetime.now()
+        results = {}
+
+        for day in range(days):
+            day_time = current_time - datetime.timedelta(days=day)
+            date_string = day_time.strftime("%Y-%m-%d")
+            rates = self.get_currencies_for(base_currency, date_string if day else 'latest')
+            results[date_string] = rates[target_currency]
+
+        return results
 
 
 def print_currencies_table_for(currency_code):
-    rates = get_currencies_for(currency_code)
+    currencies = Currencies()
+    rates = currencies.get_currencies_for(currency_code)
 
     current_time = datetime.datetime.now()
     yesterday = current_time - datetime.timedelta(days=1)
     yesterday_string = yesterday.strftime("%Y-%m-%d")
-    previous_rates = get_currencies_for(currency_code, yesterday_string)
+    previous_rates = currencies.get_currencies_for(currency_code, yesterday_string)
 
     def print_rates_change(currency_code, decimals: int = 2):
         print(
@@ -47,28 +68,11 @@ def print_currencies_table_for(currency_code):
     print_rates_change("btc")
 
 
-def compare_currency_rate(base_currency, target_currency, start_date, end_date):
-    start_rate = get_currencies_for(base_currency, start_date)
-    end_rate = get_currencies_for(base_currency, end_date)
-    return end_rate[target_currency] - start_rate[target_currency]
+currencies = Currencies()
 
-
-def get_rates_history(base_currency, target_currency, days=10):
-    current_time = datetime.datetime.now()
-    results = {}
-
-    for day in range(days):
-        day_time = current_time - datetime.timedelta(days=day)
-        date_string = day_time.strftime("%Y-%m-%d")
-        rates = get_currencies_for(base_currency, date_string if day else 'latest')
-        results[date_string] = rates[target_currency]
-
-    return results
-
-
-print(compare_currency_rate("rub", "tjs", "2022-02-11", "2022-02-26"))
-print(get_rates_history("rub", "tjs", days=10))
-data = load_currencies()
+print(currencies.compare_currency_rate("rub", "tjs", "2022-02-11", "2022-02-26"))
+print(currencies.get_rates_history("rub", "tjs", days=10))
+data = currencies.load_currencies()
 
 # for key, value in data.items():
 #     print(key.upper(), value)
---
+++ homeworks/day8.md
@@ -0,0 +1,39 @@
+# Конвертер валют
+
+## Уровень 1.
+
+Перенести все наши функции 
+`load_currencies`
+`get_currencies_for`
+`convert_currencies`
+`compare_currency_rate`
+`get_rates_history`
+
+в класс `Currencies`
+ 
+## Уровень 2:
+
+Используя класс Currencies получить даннные по курсу валют за последние 3 дня и сохранить их  в виде CSV.
+```
+USD  | 2022-02-11  | 2022-02-12 | 2022-02-13 |
+USD  |  1          |  1         |  1
+TJS  | 11.28       | 11.28      | 11.29
+RUB  | 75.6        |  76.64     | 79.4   
+
+```
+
+## Уровень 3
+Создать класс APIClient и перенести логику работы с GET и POST запросами в неё
+
+примеры использования
+```
+client = APIClient(
+   base_url="https://cdn.jsdelivr.net"
+)
+
+data = client.get(
+    path="/gh/fawazahmed0/currency-api@1/latest/currencies.json"
+)
+
+
+```
