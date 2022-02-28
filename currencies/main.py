aae92b8247256996118ddbc137bb323d7f439f43
Hasan <istamqulov@gmail.com>
2022-02-28 14:50:22 +0500

    Added homework for day 6

--- currencies/main.py
+++ currencies/main.py
@@ -12,6 +12,7 @@ def load_currencies():
 
 def get_currencies_for(currency_code, date: str = "latest"):
     url = CURRENCY_URL % (date, currency_code)
+
     data = requests.get(url).json()
     return data[currency_code]
 
@@ -46,21 +47,30 @@ def print_currencies_table_for(currency_code):
     print_rates_change("btc")
 
 
+def compare_currency_rate(base_currency, target_currency, start_date, end_date):
+    start_rate = get_currencies_for(base_currency, start_date)
+    end_rate = get_currencies_for(base_currency, end_date)
+    return end_rate[target_currency] - start_rate[target_currency]
+
+
+def get_rates_history(base_currency, target_currency, days=10):
+    current_time = datetime.datetime.now()
+    results = {}
+
+    for day in range(days):
+        day_time = current_time - datetime.timedelta(days=day)
+        date_string = day_time.strftime("%Y-%m-%d")
+        rates = get_currencies_for(base_currency, date_string if day else 'latest')
+        results[date_string] = rates[target_currency]
+
+    return results
+
+
+print(compare_currency_rate("rub", "tjs", "2022-02-11", "2022-02-26"))
+print(get_rates_history("rub", "tjs", days=10))
 data = load_currencies()
 
 # for key, value in data.items():
 #     print(key.upper(), value)
-codes = data.keys()
-print(print_currencies_table_for('tjs'))
-# print(convert_currencies('usd', 'tjs', 200))
-
-# while True:
-#     print("Пожалуйста выберите валюту")
-#     user_input = input()
-#     user_input = user_input.lower().strip()
-#     print(user_input)
-#     if user_input in codes:
-#         print("Correct")
-#         print(get_currencies_for(user_input))
-#     else:
-#         print("Incorrect")
+# codes = data.keys()
+# print(print_currencies_table_for('rub'))
